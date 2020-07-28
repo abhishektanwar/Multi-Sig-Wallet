c7c31336fd19428743d1e126cf20ee4ee2f9ea19
abhishektanwar <abhishektanwar084@gmail.com>
2020-07-29 03:03:41 +0530

    front end 1

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, {useEffect,useState} from 'react';
+import  {getWeb3,getWallet} from './utils.js';
+import Header from './header.js';
+function App() {
+  const [web3 , setWeb3] = useState(undefined);
+  const [accounts , setAccounts] = useState(undefined);
+  const [wallet , setWallet] = useState(undefined);
+  const [approvers, setApprovers] = useState([]);
+  const [quorum, setQuorum] = useState(undefined);
+  useEffect(() => {
+    const init = async () => {
+      const web3 = getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const wallet = await getWallet(web3);
+      const approvers = await wallet.methods.getApprovers().call();
+      const quorum = await wallet.methods.quorum().call();
+      setWeb3(web3);
+      setWallet(wallet);
+      setAccounts(accounts);
+      setApprovers(approvers);
+      setQuorum(quorum);
+    };
+    init();
+  },[]);
+
+  // if(typeof web3 === 'undefined' 
+  //   || typeof accounts === 'undefined' 
+  //   || typeof wallet === 'undefined' 
+  //   || approvers.length === 0 
+  //   || typeof quorum === 'undefined'){
+  //   return(
+  //     <div>Loading....</div>
+  //   )
+  // }
+  return (
+    <div>
+      MultiSig Dapp
+      <Header approvers={approvers} quorum={quorum}></Header>
+    </div>
+  );
+}
+
+export default App;
